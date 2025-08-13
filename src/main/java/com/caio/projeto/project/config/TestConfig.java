75b81a98bf4d9224648f2d5e778e72ee42d525ff
CaioQC <caioq2004@gmail.com>
2025-08-13 18:28:48 -0300

    Payment, one-to-one association

--- src/main/java/com/caio/projeto/project/config/TestConfig.java
+++ src/main/java/com/caio/projeto/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.caio.projeto.project.entities.Category;
 import com.caio.projeto.project.entities.Order;
 import com.caio.projeto.project.entities.OrderItem;
+import com.caio.projeto.project.entities.Payment;
 import com.caio.projeto.project.entities.Product;
 import com.caio.projeto.project.entities.User;
 import com.caio.projeto.project.entities.enums.OrderStatus;
@@ -77,5 +78,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
